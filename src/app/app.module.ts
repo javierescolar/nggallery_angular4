183ca5da688c4517a4565db2aee23812b9cee1fc
Javier Escolar Salcedo <javierescolar10@gmail.com>
2017-10-02 21:18:39 +0200

    videotutorial 14

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageListComponent } from './gallery/image-list/image-list.component';
+import { ImageComponent } from './gallery/image-list/image.component';
+
+import { ImageService } from './services/image.service';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    GalleryComponent,
+    ImageListComponent,
+    ImageComponent,
+    ImageDetailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [ImageService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/gallery/image-list/image-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Image } from '../../models/image';
+import { ImageService } from '../../services/image.service';
+
+@Component({
+  selector: 'ng-image-list',
+  templateUrl: './image-list.component.html',
+  styles: []
+})
+export class ImageListComponent implements OnInit {
+  images: Image[] = [];
+  selectedImage: Image;
+  constructor(private imageService: ImageService) { }
+
+  ngOnInit() {
+    this.images = this.imageService.getImages();
+  }
+
+  onSelect(image: Image) {
+    this.selectedImage = image;
+    console.log(this.selectedImage);
+  }
+}
